49fd60de2fd5c18dff5255fc39f41e7c1c24b85c
Andrew Scott <andrewscott13@mittymonarch.com>
2016-01-05 08:18:07 +0000

    functionality now includes showing a preview of where the next piece will go and then putting it there when clicked

--- othello.py
+++ othello.py
@@ -7,24 +7,35 @@ import random
 from pygame.locals import *
 from pygame import *
 
+# colors
+black = (0, 0, 0)
+white = (255, 255, 255)
+green  = (100, 140, 100)
+
 class Othello:
     
     def __init__(self):
         self.size = 80
         self.displaySize = (self.size * 8, self.size * 8)
         pygame.init()
+        self.clock = pygame.time.Clock()
         display.set_caption("Othello")
         self.display = display.set_mode(self.displaySize)
         #self.background = Background(self.displaySize)
 
         # True for player1, False for player2
         self.currentPlayer = True
-        self.board = [[Square(80) for x in range(8)] for x in range(8)]
+        self.board = [[0 for x in range(8)] for x in range(8)]
 
         self.mouseLoc = (0, 0)
         self.movePreview = (0, 0)
-    def handleEvents(self):
+        self.player = 1 # 1 for white, 2 for black
+        self.board[3][3] = 1
+        self.board[3][4] = 2
+        self.board[4][3] = 2
+        self.board[4][4] = 1
         
+    def handleEvents(self):
         # Handle events, starting with the quit event
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -35,53 +46,56 @@ class Othello:
                 self.mouseLoc = event.pos
                 self.movePreview = (self.mouseLoc[0] / self.size,
                                     self.mouseLoc[1] / self.size)
+
+            if event.type == MOUSEBUTTONDOWN:
+                if self.canFlip(self.player, self.movePreview):
+                    self.board[self.movePreview[0]][self.movePreview[1]] = self.player
+                    if self.player == 1:
+                        self.player = 2
+                    else:
+                        self.player = 1
+
+
+
+    def canFlip(self, player, movePreview):
+        return True
+
     def update(self):
-        display.fill((100, 145, 100))
-# The class for the background
-#class Background:
-    
-#    def __init__(self, displaySize):
+        self.display.fill(green)
+        self.drawSquares()
+        self.drawPreview()
         
-        # Set our image to a new surface, the size of the screen rectangle
-#        self.image = Surface(displaySize)
-        
-        # Fill the image with a green colour (specified as R,G,B)
-#        self.image.fill((27, 210, 57))
-        
-        # Get width proportionate to display size
-#        lineWidth = displaySize[0] / 80
-        
-        # Create a rectangle to make the white line
- #       lineRect = Rect(0, 0, lineWidth, displaySize[1])
-  #      lineRect.centerx  = displaySize[0] / 2
-   #     draw.rect(self.image, (255, 255, 255), lineRect)
-        
-    #def draw(self, display):
+    def run(self):
+        # Runs the game loop
+        while True:
+            self.handleEvents()
+            self.update()
+            # Update the full display surface to the screen
+            display.update()            
+            # Limit the game to 30 frames per second
+            self.clock.tick(30)
+
+    def drawPreview(self):
+        if self.board[self.movePreview[0]][self.movePreview[1]] == 0:               
+            center = (self.movePreview[0] * self.size + self.size / 2,
+                      self.movePreview[1] * self.size + self.size / 2)
+            if self.player == 1:
+                draw.circle(self.display, white, center, self.size / 2)
+            else:
+                draw.circle(self.display, black, center, self.size / 2)
             
-        # Draw the background to the display that has been passed in
-     #   display.blit(self.image, (0,0))
-
-
-class Square:
-    """
-    Board square class. The color data member tells us what is in the square
-    currently (0 = empty, 1 = white, 2 = black).
-    """
-    def __init__(self, size):
-        self.color = 0
-        self.size = size
-
-    def getColor(self):
-        return self.color
-
-    def flip(self):
-        if self.color == 1:
-            self.color = 2
-        else:
-            self.color = 1
-
-    def setColor(self, color):
-        self.color = color
+    def drawSquares(self):
+        for i in range(8):
+            for j in range(8):
+                square = Rect(i * self.size, j * self.size, self.size, self.size)
+                draw.rect(self.display, black, square, 1)
+                center = (i * self.size + self.size / 2, j * self.size +
+                          self.size / 2)
+                if self.board[i][j] == 1:
+                    draw.circle(self.display, white, center, self.size / 2)
+                if self.board[i][j] == 2:
+                    draw.circle(self.display, black, center, self.size / 2)
             
 if __name__ == '__main__':
     game = Othello()
+    game.run()
